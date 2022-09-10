d831846b39e079f128ddc1f48d4903aae0eb609c
Indian Tech Programmer <112259649+IndianTechProgrammer123@users.noreply.github.com>
2022-09-10 14:20:49 +0530

    Add files via upload

---
+++ Variable2.py
@@ -0,0 +1,17 @@
+# How to write multiple variables in a single line 
+
+# I have three variables A, B, C
+A = 19
+B = 48.4
+C = 'hello'
+
+# I want the variables to be written in a single line
+A , B = 19 , 48.4
+print(A)
+print(A,B)
+
+A , B, C = 19 , 48.4 , 'hello'
+print(A)
+print(A,B)
+print(A,B,C)
+
